2fdd825e824c69e5c03ff999a6b38d8567c44d20
DrFariz <nurrohmna129@gmail.com>
2025-05-02 17:47:16 +0800

    no sandbox

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
   await browser.close();
 })();
